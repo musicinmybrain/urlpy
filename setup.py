a99617184803c29f6c8bf745a567e17b40e03e10
pombredanne <pombredanne@nexb.com>
2015-06-04 15:43:42 -0700

    bump

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 
 setup(
 	name             = 'url',
-	version          = '0.1.4.2',
+	version          = '0.1.4.3',
 	description      = 'URL Parsing',
 	long_description = '''
 Some helper functions for parsing URLs, sanitizing them, normalizing them.
